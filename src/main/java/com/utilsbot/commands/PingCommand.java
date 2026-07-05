e84ff5b103c77b811bd35b7b841338c9cf232976
BalencedVeil717 <105261700+BalencedVeil717@users.noreply.github.com>
2026-07-05 14:50:33 +0530

    [REFactor] Converted Ping command to a Container using Components v2
    [REFactor] Added ping quality and time in ping command response

--- src/main/java/com/utilsbot/commands/PingCommand.java
+++ src/main/java/com/utilsbot/commands/PingCommand.java
@@ -1,14 +1,43 @@
 package com.utilsbot.commands;
 
+import net.dv8tion.jda.api.components.container.Container;
+import net.dv8tion.jda.api.components.separator.Separator;
+import net.dv8tion.jda.api.components.textdisplay.TextDisplay;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 public class PingCommand extends ListenerAdapter {
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         if (event.getName().equals("ping")) {
             long ping = event.getJDA().getGatewayPing();
-            event.reply("Ping : " + ping + "ms").queue();
+            String label =
+                ping <= 50 ? "Excellent" :
+                ping <= 100 ? "Great" :
+                ping <= 150 ? "Good" :
+                ping <= 250 ? "Fair" :
+                ping <= 400 ? "Poor" :
+                ping <= 500 ? "Bad" : "Terrible";
+
+            String time = DateTimeFormatter.ofPattern("dd MMM, HH:mm").withZone(ZoneOffset.UTC).format(Instant.now());
+
+
+            event
+                    .reply("")
+                    .useComponentsV2()
+                    .addComponents(
+                            Container.of(
+                                    TextDisplay.of("## Ping : " + ping + " ms"),
+                                    Separator.createDivider(Separator.Spacing.SMALL),
+                                    TextDisplay.of("-# " + label + " | " + time + " UTC")
+
+                            )
+                    )
+                    .queue();
         }
     }
 }
